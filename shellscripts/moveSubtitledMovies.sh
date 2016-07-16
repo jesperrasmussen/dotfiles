1efc94b73bc60b97cdd447e156088e25e001e05c
Jesper Rasmussen <me@jesperrasmussen.com>
2016-07-17 00:02:10 +0200

    Updates script with proper find command

--- shellscripts/moveSubtitledMovies.sh
+++ shellscripts/moveSubtitledMovies.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd /media/Movies/Uncategorized
-find -name "*.da.srt" | xargs -I{} dirname {} | xargs -I list mv list /media/Movies/Subtitles
-
+find . -name "*.da.srt" -exec sh -c 'mv "${0%/*}" ../Subtitles/' {} \;
