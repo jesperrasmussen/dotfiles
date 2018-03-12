ac4538213edbf0951b49dc224cf0757711b8ba6f
Jesper Rasmussen <me@jesperrasmussen.com>
2018-03-12 22:25:39 +0100

    Updates mount for TV 2

--- shellscripts/workMount.sh
+++ shellscripts/workMount.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-umount /Volumes/youseetv
-rm /Volumes/youseetv
-mkdir /Volumes/youseetv
-sshfs -p 22 jr@d-ott01:/var/www/jr/youseetv /Volumes/youseetv -oauto_cache,reconnect,defer_permissions,negative_vncache,volname=youseetv
+umount ~/mnt/tv2
+rm ~/mnt/tv2
+mkdir ~/mnt/tv2
+#sshfs -p 22 jr@d-ott01:/var/www/jr/youseetv /Volumes/youseetv -oauto_cache,reconnect,defer_permissions,negative_vncache,volname=youseetv
+sshfs jesr@dn-test-php5.i.tv2.dk: ~/mnt/tv2 -oauto_cache,reconnect,defer_permissions,negative_vncache,volname=tv2
